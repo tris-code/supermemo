19870e32958d0befbf308c0a28ff63b9c74ad9e8
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-09-18 14:44:10 +0000

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:4.2
+/******************************************************************************
+ *                                                                            *
+ * Tris Foundation disclaims copyright to this source code.                   *
+ * In place of a legal notice, here is a blessing:                            *
+ *                                                                            *
+ *     May you do good and not evil.                                          *
+ *     May you find forgiveness for yourself and forgive others.              *
+ *     May you share freely, never taking more than you give.                 *
+ *                                                                            *
+ ******************************************************************************/
+
+import PackageDescription
+
+let package = Package(
+    name: "SuperMemo",
+    products: [
+        .library(
+            name: "SuperMemo",
+            targets: ["SuperMemo"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/tris-foundation/test.git",
+            .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "SuperMemo",
+            dependencies: []),
+        .testTarget(
+            name: "SuperMemoTests",
+            dependencies: ["SuperMemo", "Test"]),
+    ]
+)
